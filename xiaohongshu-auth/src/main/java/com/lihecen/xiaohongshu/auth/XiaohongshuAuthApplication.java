0638d77e79bb9fdc0eaa572620cb126c90542444
Lihecen <2837842053@qq.com>
2025-10-04 17:27:05 +0800

    Initial commit: 微服务项目搭建

---
+++ xiaohongshu-auth/src/main/java/com/lihecen/xiaohongshu/auth/XiaohongshuAuthApplication.java
@@ -0,0 +1,11 @@
+package com.lihecen.xiaohongshu.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohongshuAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuAuthApplication.class, args);
+    }
+}
