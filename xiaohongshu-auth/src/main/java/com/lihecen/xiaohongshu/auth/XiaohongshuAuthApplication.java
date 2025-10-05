8803e013cab85dfa0fc1613da10439aab56146d4
Lihecen <2837842053@qq.com>
2025-10-06 04:05:38 +0800

    整合 MyBatis 框架

--- xiaohongshu-auth/src/main/java/com/lihecen/xiaohongshu/auth/XiaohongshuAuthApplication.java
+++ xiaohongshu-auth/src/main/java/com/lihecen/xiaohongshu/auth/XiaohongshuAuthApplication.java
@@ -1,9 +1,11 @@
 package com.lihecen.xiaohongshu.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lihecen.xiaohongshu.auth.domain.mapper")
 public class XiaohongshuAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(XiaohongshuAuthApplication.class, args);
--- xiaohongshu-auth/src/test/java/com/lihecen/xiaohongshu/auth/XiaohongshuAuthApplicationTests.java
+++ xiaohongshu-auth/src/test/java/com/lihecen/xiaohongshu/auth/XiaohongshuAuthApplicationTests.java
@@ -1,13 +1,56 @@
 package com.lihecen.xiaohongshu.auth;
 
+import com.lihecen.framework.common.util.JsonUtils;
+import com.lihecen.xiaohongshu.auth.domain.dataobject.UserDO;
+import com.lihecen.xiaohongshu.auth.domain.mapper.UserDOMapper;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class XiaohongshuAuthApplicationTests {
+import java.time.LocalDateTime;
 
+@SpringBootTest
+@Slf4j
+class XiaohongshuAuthApplicationTests {
+    @Resource
+    private UserDOMapper userDOMapper;
+
+    /**
+     * 测试插入数据
+     */
     @Test
-    void contextLoads() {
+    void testInsert() {
+        UserDO userDO = UserDO.builder().username("栗赫岑").createTime(LocalDateTime.now()).updateTime(LocalDateTime.now()).build();
+        userDOMapper.insert(userDO);
     }
 
+    /**
+     * 查询数据
+     */
+    @Test
+    void testSelect() {
+        // 查询主键 ID 为 1 的记录
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1L);
+        log.info("User: {}", JsonUtils.toJsonString(userDO));
+    }
+
+    /**
+     * 更新数据
+     */
+    @Test
+    void testUpdate() {
+        UserDO userDO = UserDO.builder().id(1L).username("lihecen").updateTime(LocalDateTime.now()).createTime(LocalDateTime.now()).build();
+        //　根据主键 ID 更新记录
+        userDOMapper.updateByPrimaryKey(userDO);
+    }
+
+    /**
+     * 删除数据
+     */
+    @Test
+    void testDelete() {
+        //　删除主键 ID　为 1 的记录
+        userDOMapper.deleteByPrimaryKey(1L);
+    }
 }
