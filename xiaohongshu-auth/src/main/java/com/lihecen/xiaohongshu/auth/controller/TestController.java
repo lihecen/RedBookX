eb1afe3fd6569ab884e7df8096b2c460bd360f24
Lihecen <2837842053@qq.com>
2025-10-07 04:54:52 +0800

    初试 SaToken 权限认证框架

--- xiaohongshu-auth/src/main/java/com/lihecen/xiaohongshu/auth/controller/TestController.java
+++ xiaohongshu-auth/src/main/java/com/lihecen/xiaohongshu/auth/controller/TestController.java
@@ -1,14 +1,10 @@
 package com.lihecen.xiaohongshu.auth.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.lihecen.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.lihecen.framework.common.response.Response;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
@@ -23,5 +19,22 @@ public class TestController {
     public Response<User> test2(@RequestBody @Validated User user) {
        int i = 1 / 0;
        return Response.success(user);
+    }
+
+    // 测试登录，浏览器访问： http://localhost:8080/user/doLogin?username=zhang&password=123456
+    @RequestMapping("/user/doLogin")
+    public String doLogin(String username, String password) {
+        // 模拟示例
+        if("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+            return "登录成功";
+        }
+        return "登录失败";
+    }
+
+    // 查询登录状态，浏览器访问： http://localhost:8080/user/isLogin
+    @RequestMapping("/user/isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
     }
 }
