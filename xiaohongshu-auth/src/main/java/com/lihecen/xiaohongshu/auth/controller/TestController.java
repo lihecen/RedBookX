7dffcabfa1a7d6a893cecc2b20da80ef1f8d2afd
Lihecen <2837842053@qq.com>
2025-10-06 22:41:25 +0800

    添加全局异常捕获和接口参数校验

--- xiaohongshu-auth/src/main/java/com/lihecen/xiaohongshu/auth/controller/TestController.java
+++ xiaohongshu-auth/src/main/java/com/lihecen/xiaohongshu/auth/controller/TestController.java
@@ -2,7 +2,10 @@ package com.lihecen.xiaohongshu.auth.controller;
 
 import com.lihecen.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.lihecen.framework.common.response.Response;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -15,9 +18,10 @@ public class TestController {
         return Response.success("Hello");
     }
 
-    @GetMapping("/test2")
+    @PostMapping("/test2")
     @ApiOperationLog(description = "测试接口2")
-    public Response<User> test2() {
-        return Response.success(User.builder().nickname("lihecen").createTime(LocalDateTime.now()).build());
+    public Response<User> test2(@RequestBody @Validated User user) {
+       int i = 1 / 0;
+       return Response.success(user);
     }
 }
