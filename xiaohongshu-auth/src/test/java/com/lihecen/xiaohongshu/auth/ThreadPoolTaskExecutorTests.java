d609386b0ee5b43e62f087c6fd59c531a8f1a3e2
Lihecen <2837842053@qq.com>
2025-10-07 23:36:45 +0800

    实现自定义线程池

---
+++ xiaohongshu-auth/src/test/java/com/lihecen/xiaohongshu/auth/ThreadPoolTaskExecutorTests.java
@@ -0,0 +1,22 @@
+package com.lihecen.xiaohongshu.auth;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@SpringBootTest
+@Slf4j
+public class ThreadPoolTaskExecutorTests {
+   @Resource
+   private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+    /**
+     * 测试线程池
+     */
+    @Test
+    public void testSubmit() {
+        threadPoolTaskExecutor.submit(() -> log.info("异步线程中说: 栗赫岑"));
+    }
+}
